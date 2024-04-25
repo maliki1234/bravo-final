68d982cbb9b662d45ac04ed632b673e1980f871a
maliki1234 <malikikowero@gmail.com>
2024-04-25 11:49:55 +0300

    change prisma generate

--- app/customize/layout.tsx
+++ app/customize/layout.tsx
@@ -12,7 +12,9 @@ import SideBar from "@/components/sidebar/SideBar"
 import { Toaster } from "@/components/ui/toaster"
 // import Header from "@/components/product/Header"
 import Link from "next/link"
-import Header from "@/components/product/Header"
+import Header from "@/components/header/Header"
+import { report } from "@/lib/link"
+// import Header from "@/components/product/Header"
 // import Header from "@/components/header/Header"
 
 
@@ -47,13 +49,13 @@ export default async function RootLayout({
       <Provider>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
-        <div className="grid grid-cols-9">
-          <div className="col-span-1">
+        <div className="grid grid-cols-1 md:grid-cols-9">
+          <div className="hidden md:block col-span-1">
            <SideBar />
           </div>
-          <div className="col-span-8">
+          <div className="col-span-1 md:col-span-8">
             <div className='relative h-screen'>
-            <Header/>
+            <Header links={report}/>
               {children}
             </div >
           </div>
