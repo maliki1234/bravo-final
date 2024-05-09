abf740596a2dad87c9368d26188134a63afc32f6
maliki1234 <malikikowero@gmail.com>
2024-05-09 11:04:23 +0300

    first comit

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   reactStrictMode: true, // Enable React strict mode for improved error handling
   swcMinify: true,      // Enable SWC minification for improved performance
   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    // removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
   eslint: {
       
